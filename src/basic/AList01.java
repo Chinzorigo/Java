ade2f8c6be18b0566749c19c4c22a61383ba8b17
chinzorig <chinzorig.o@gmail.com>
2022-06-10 23:47:36 +0800

    Basics of Java.iml

---
+++ src/basic/AList01.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.ArrayList;
+
+public class AList01 {
+    public static void main(String[] args) {
+        ArrayList<String> languages = new ArrayList<>();
+
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("Swift");
+        System.out.println("Languages: " + languages);
+    }
+}
