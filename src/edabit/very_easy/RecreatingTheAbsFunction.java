907ec8d79f09379b4cd98dc8282fe2e63fad48fa
chinzorig <chinzorig.o@gmail.com>
2022-07-02 23:43:27 +0800

    Basics of Java.iml

---
+++ src/edabit/very_easy/RecreatingTheAbsFunction.java
@@ -0,0 +1,12 @@
+package edabit.very_easy;
+
+public class RecreatingTheAbsFunction {
+    public static void main(String[] args) {
+        double n = -1.23456;
+        System.out.println(absolute(n));
+    }
+
+    public static double absolute(double n) {
+        return Math.abs(n);
+    }
+}
